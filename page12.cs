77ce1789ef3f91336865433dd831531ce89ce094
cls <you@example.com>
2017-12-19 14:55:32 +0700

    This is Myproject1

---
+++ page12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName = "Yoottana";
+
+            uint yourAge = 21;
+
+            Console.WriteLine("Hello {0}. You are {1} years old.", yourName, yourAge);
+            Console.ReadLine();
+        }
+    }
+}
